de5cc7feeca4d47d54f42e3f69bce85976f4877f
Rodrigo Antunes <rodrigoantunestutz@gmail.com>
2022-07-17 03:26:55 -0300

    Initial Commit

---
+++ src/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("MAIL", [
+    "host" => 'smtp-teste.com',
+    "port" => '587',
+    "user" => 'seu-usuario.com',
+    "passwd" => 'Sua senha',
+    "from_name" => 'Seu Nome',
+    "from_email" => 'seuemail@teste.com',
+]);
---
+++ src/Mail.php
@@ -0,0 +1,63 @@
+<?php 
+
+namespace RodrigoTutz\SendEmail;
+
+use Exception;
+use stdClass;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mail {
+
+    private $mail;
+    private $data;
+    private $error;
+
+    public function __construct(){
+        $this->mail = new PHPMailer(true);
+        $this->data = new stdClass();
+
+        $this->mail->isSMTP();
+        $this->mail->isHTML();
+        $this->mail->setLanguage('br');
+
+        $this->mail->SMTPAuth = true;
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->CharSet = 'utf-8';
+
+        $this->mail->Host = MAIL['host'];
+        $this->mail->Port = MAIL['port'];
+        $this->mail->Username = MAIL['user'];
+        $this->mail->Password = MAIL['passwd'];
+    }
+
+    public function add($subject, $body, $recipient_name, $recipient_email) : Mail {
+        $this->data->subject = $subject;
+        $this->data->body = $body;
+        $this->data->recipient_name = $recipient_name;
+        $this->data->recipient_email = $recipient_email;
+
+        return $this;
+    }
+
+    public function send($from_name = MAIL['from_name'], $from_email = MAIL['from_email']) : bool{
+
+        try {
+
+            $this->mail->Subject = $this->data->subject;
+            $this->mail->msgHTML($this->data->body);
+            $this->mail->addAddress( $from_email, $from_name);
+            $this->mail->setFrom($this->data->recipient_email, $this->data->recipient_name);
+
+            $this->mail->send();
+            return true;
+
+        }catch(Exception $exeception) {
+            $this->error = $exeception;
+            return false;
+        }
+    }
+
+    public function error() : ? Exception {
+        return $this->error;
+    }
+}
